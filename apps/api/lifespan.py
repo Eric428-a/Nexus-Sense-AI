ec6222c3a22e36117deaf739294c1cce6ad221a8
Eric Mwaniki M. <ericmwaniki2004@gmail.com>
2026-09-04 22:48:44 +0300

    Add lifespan context manager for FastAPI app

---
+++ apps/api/lifespan.py
@@ -0,0 +1,8 @@
+"""API application lifecycle."""
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    app.state.ready=True
+    yield
+    app.state.ready=False
